96fb0491c4202d17fb4a99df83c8209f07493bf8
zhengliang19 <42111185+zhengliang19@users.noreply.github.com>
2018-08-05 18:34:12 +0800

    Update main.py to include buff casting function

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 # 2. Auto Change Channel                                #
 # 3. Auto Join Dungeon                                  #
 # 4. Properly define the list of keys                   #
-# 5. ???                                                #
+# 5. Solve key conflict issues when casting buff + skill#
+# 6. ???                                                #
 #                                                       #
 #########################################################
 
@@ -23,13 +24,16 @@ import random
 
 directions = 1
 
+# The number of set of skills your character has. 
+noOfSkillsSet = 1 
+
 def keyPress(keyValue):
 	press(keyValue)
 	print ("'" + keyValue + "'" + " key pressed!")
 	
 	# To prevent keyboard input delay:
 	time.sleep(0.5)
-	
+
 def skillCast():
 	global directions
 	rand = [1, 2, 3, 4, 5]	
@@ -46,11 +50,22 @@ def skillCast():
 		keyPress('a') #keyPress('left')
 		keyPress('a') #keyPress('left')
 	directions = not directions
+
+# Modify this accordingly based on your skill key bindings
+def buffCast():
+	# Cast buffs every 30 mins
+	global noOfSkillsSet
+	threading.Timer(10, buffCast).start()
+	buffKeys = ['1', '2', '3', '4']
+	switchKey = 'f1'
+	for x in range(noOfSkillsSet): # 3 Sets of skill key bindings
+		for keyValue in buffKeys:
+			keyPress(keyValue)
+		if (noOfSkillsSet != 1):
+			keyPress(switchKey)
 	
 print ("Program Started.")
 print ("Press Ctrl + C to exit.")
+buffCast()
 while True:
 	skillCast()
-	
-	
-	
